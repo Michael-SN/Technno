e502defe0cc2672aa0c15c366799a42af299bb29
Michael SN <michael.dev.nascimento@gmail.com>
2022-07-18 22:54:12 -0300

    Techno Project

---
+++ app.js
@@ -0,0 +1,61 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    products: [],
+    itemProduct: false,
+  },
+  filters: {
+    formatPrice(value) {
+      return value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
+    capitalizer(value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    },
+  },
+  methods: {
+    fetchProducts() {
+      fetch("./api/produtos.json")
+        .then((res) => res.json())
+        .then((rjson) => {
+          this.products = rjson;
+        })
+        .catch(function (error) {
+          console.error(
+            "There has been a problem with your fetch operation: " +
+              error.message
+          );
+        });
+    },
+    fetchItemProduct(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((response) => response.json())
+        .then((rjson) => {
+          this.itemProduct = rjson;
+        })
+        .catch(function (error) {
+          console.error(
+            "There has been a problem with your fetch operation: " +
+              error.message
+          );
+        });
+    },
+    openModal(id) {
+      this.fetchItemProduct(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+  },
+  beforeCreate() {
+    document.title = "Techno Project";
+  },
+  created() {
+    this.fetchProducts();
+  },
+});
